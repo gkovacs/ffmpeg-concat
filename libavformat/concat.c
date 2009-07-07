7778d682f652402a6e1722e4c9cddb3a968782ce
Geza Kovacs <gkovacs@mit.edu>
2009-07-06 18:04:59 -0700

    aligned variable assignment list

--- libavformat/concat.c
+++ libavformat/concat.c
@@ -41,24 +41,24 @@ static int concat_read_header(AVFormatContext *s,
 
 void concat_init_demuxer(AVInputFormat *cdm)
 {
-    cdm->name = "concat";
-    cdm->long_name = NULL_IF_CONFIG_SMALL("CONCAT format");
+    cdm->name           = "concat";
+    cdm->long_name      = NULL_IF_CONFIG_SMALL("CONCAT format");
     cdm->priv_data_size = sizeof(PlaylistContext);
-    cdm->read_probe = concat_probe;
-    cdm->read_header = concat_read_header;
-    cdm->read_packet = ff_concatgen_read_packet;
-    cdm->read_close = ff_concatgen_read_close;
-    cdm->read_seek = ff_concatgen_read_seek;
+    cdm->read_probe     = concat_probe;
+    cdm->read_header    = concat_read_header;
+    cdm->read_packet    = ff_concatgen_read_packet;
+    cdm->read_close     = ff_concatgen_read_close;
+    cdm->read_seek      = ff_concatgen_read_seek;
     cdm->read_timestamp = ff_concatgen_read_timestamp;
-    cdm->flags = NULL;
-    cdm->extensions = NULL;
-    cdm->value = NULL;
-    cdm->read_play = ff_concatgen_read_play;
-    cdm->read_pause = ff_concatgen_read_pause;
-    cdm->codec_tag = codec_concat_tags;
-    cdm->read_seek2 = ff_concatgen_read_seek;
-    cdm->metadata_conv = NULL;
-    cdm->next = NULL;
+    cdm->flags          = NULL;
+    cdm->extensions     = NULL;
+    cdm->value          = NULL;
+    cdm->read_play      = ff_concatgen_read_play;
+    cdm->read_pause     = ff_concatgen_read_pause;
+    cdm->codec_tag      = codec_concat_tags;
+    cdm->read_seek2     = ff_concatgen_read_seek;
+    cdm->metadata_conv  = NULL;
+    cdm->next           = NULL;
 }
 
 #if CONFIG_CONCAT_DEMUXER
