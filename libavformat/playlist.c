129a4a1bbbd9c76fa4c827f14a2ff97f434ce6f7
Geza Kovacs <gkovacs@mit.edu>
2009-07-11 23:57:46 -0700

    identifier for concat demuxers, open codec in init_playelem

--- libavformat/playlist.c
+++ libavformat/playlist.c
@@ -57,7 +57,6 @@ void ff_playlist_init_playelem(PlayElem *pe)
     }
     
     for (i = 0; i < pe->ic->nb_streams; ++i) {
-//        pe->ic->streams[i]->codec->codec = 0;
         AVCodec *codec = avcodec_find_decoder(pe->ic->streams[i]->codec->codec_id);
         if (!codec) {
             fprintf(stderr, "output_packet: Decoder (codec id %d) not found for input stream #%d\n",
