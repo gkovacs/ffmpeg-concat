3dd04400c5f7d86c43ce39a728c6a2f416ddbdad
Geza Kovacs <gkovacs@mit.edu>
2009-07-23 17:56:15 -0700

    ensure that flist and icl are always null-terminated

--- libavformat/playlist.c
+++ libavformat/playlist.c
@@ -53,6 +53,7 @@ void ff_playlist_populate_context(AVFormatContext *s)
     AVFormatContext *ic;
     PlaylistContext *ctx = s->priv_data;
     ctx->icl = av_realloc(ctx->icl, sizeof(*(ctx->icl)) * (ctx->pe_curidx+1));
+    ctx->icl[ctx->pe_curidx+1] = NULL;
     ic = ctx->icl[ctx->pe_curidx] = ff_playlist_alloc_formatcontext(ctx->flist[ctx->pe_curidx]);
     ic->iformat->read_header(ic, 0);
     s->nb_streams = ic->nb_streams;
@@ -134,6 +135,7 @@ PlaylistContext *ff_playlist_from_encodedstring(char *s, char sep)
 void ff_playlist_add_path(PlaylistContext *ctx, char *itempath)
 {
     ctx->flist = av_realloc(ctx->flist, sizeof(*(ctx->flist)) * ++ctx->pelist_size);
+    ctx->flist[ctx->pelist_size] = NULL;
     ctx->flist[ctx->pelist_size-1] = itempath;
 }
 
