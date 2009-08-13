544a3e61f698b3efd8101021cd0532a1a6326197
Geza Kovacs <gkovacs@mit.edu>
2009-08-13 12:57:24 -0700

    mpeg1video reports b_frames but requires dts and pts to be offset

--- libavformat/concatgen.c
+++ libavformat/concatgen.c
@@ -54,7 +54,8 @@ int ff_concatgen_read_packet(AVFormatContext *s,
             if (pkt) {
                 stream_index = ff_playlist_localstidx_from_streamidx(ctx, pkt->stream_index);
                 pkt->stream_index = stream_index + ff_playlist_streams_offset_from_playidx(ctx, ctx->pe_curidx);
-                if (!ic->streams[stream_index]->codec->has_b_frames) {
+                if (!ic->streams[stream_index]->codec->has_b_frames ||
+                    ic->streams[stream_index]->codec->codec->id == CODEC_ID_MPEG1VIDEO) {
                     pkt->dts += av_rescale_q(ff_playlist_time_offset(ctx->durations, ctx->pe_curidx),
                                              AV_TIME_BASE_Q,
                                              ic->streams[stream_index]->time_base);
