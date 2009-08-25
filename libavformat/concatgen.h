fba37f0e5170e04b10821fa53659a1969872a3e1
Geza Kovacs <gkovacs@mit.edu>
2009-08-24 22:41:47 -0700

    doxy comments for functions in concatgen.h

--- libavformat/concatgen.h
+++ libavformat/concatgen.h
@@ -24,22 +24,54 @@
 
 #include "libavformat/avplaylist.h"
 
+/** @brief Generic implementation of read_packet for concat demuxers.
+ *  @details Read one packet and put it in 'pkt'. pts and flags are also
+ *  set. 'av_new_stream' can be called only if the flag
+ *  AVFMTCTX_NOHEADER is used.
+ *  @return  0 on success, < 0 on error.
+ *  When returning an error, pkt must not have been allocated
+ *  or must be freed before returning
+ */
 int ff_concatgen_read_packet(AVFormatContext *s, AVPacket *pkt);
 
+/** @brief Generic implementation of read_seek for concat demuxers.
+  * @details Seek to a given timestamp relative to the frames in
+  * stream component stream_index.
+  * @param stream_index Must not be -1.
+  * @param flags Selects which direction should be preferred if no exact
+  * match is available.
+  * @return >= 0 on success (but not necessarily the new offset)
+ */
 int ff_concatgen_read_seek(AVFormatContext *s,
                            int stream_index,
                            int64_t pts,
                            int flags);
 
+/** @brief Generic implementation of read_timestamp for concat demuxers.
+ *  @details Gets the next timestamp in stream[stream_index].time_base units.
+ *  @return the timestamp or AV_NOPTS_VALUE if an error occurred
+ */
 int64_t ff_concatgen_read_timestamp(AVFormatContext *s,
                                     int stream_index,
                                     int64_t *pos,
                                     int64_t pos_limit);
 
+/** @brief Generic implementation of read_close for concat demuxers.
+ *  @details Close the stream. The AVFormatContext and AVStreams are not
+ *  freed by this function
+ */
 int ff_concatgen_read_close(AVFormatContext *s);
 
+/** @brief Generic implementation of read_play for concat demuxers.
+ *  @details Starts/resumes playing - only meaningful if using a network-based
+ *  format (RTSP).
+ */
 int ff_concatgen_read_play(AVFormatContext *s);
 
+/** @brief Generic implementation of read_pause for concat demuxers.
+ *  @details Pauses playing - only meaningful if using a network-based
+ *  format (RTSP).
+ */
 int ff_concatgen_read_pause(AVFormatContext *s);
 
 #endif /* AVFORMAT_CONCATGEN_H */
